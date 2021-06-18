1cf123a7469eab61a567e3b79fbb9542d8995ea7
drStacky <dr.stacky@gmail.com>
2021-06-18 17:03:10 +0000

    Remove profile

--- build_docker.sh
+++ build_docker.sh
@@ -4,6 +4,6 @@ echo 'Building image'
 sudo nvidia-docker build -f Dockerfile -t rad_ml:latest .
 sudo docker tag rad_ml:latest 769003761693.dkr.ecr.us-east-1.amazonaws.com/mstackpo/rad_ml:latest
 echo 'Signing into AWS'
-sudo $(aws --profile rdml ecr get-login --no-include-email --region us-east-1)
+sudo $(aws ecr get-login --no-include-email --region us-east-1)
 echo "Pushing image to ECR"
 sudo docker push 769003761693.dkr.ecr.us-east-1.amazonaws.com/mstackpo/rad_ml:latest
